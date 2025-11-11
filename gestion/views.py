b8b272ba55c6675864e051e90b27a0119b478046
Franco_SJ <jean.jara.fs@gmail.com>
2025-11-11 00:46:57 -0300

    feat: crear cuenta y listar cuentas

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from gestion.servicios import cuentas
 
-# Create your views here.
+def landing_page(request):
+    return render(request, 'landing-page/landing.html')
+
+def cuenta_page(request):
+    lista_cuentas = cuentas.obtener_cuentas()
+
+    lista = {
+        "cuentas": lista_cuentas
+    }
+    return render(request, 'cuenta/cuenta-page.html', lista)
+
+def crear_cuenta(request):
+    resultado = {}
+    if request.method == 'POST':
+        respuesta = cuentas.crear_cuenta(request)
+
+        if respuesta["success"] == False:
+            resultado["success"] = False
+            resultado["mensaje"] = respuesta["mensaje"]
+        else:
+            resultado["success"] = True
+            resultado["mensaje"] = respuesta["mensaje"]
+
+        resultado["cuentas"] = cuentas.obtener_cuentas()
+
+        return render(request, 'cuenta/cuenta-page.html', resultado)
+    else:
+        resultado["cuentas"] = cuentas.obtener_cuentas()
+        return render(request, 'cuenta/cuenta-page.html', resultado)
+
+
